7c5fbd2ee378760d1fdc9e8c7fe08214ce846eea
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-27 21:38:31 +0200

    feat(search): add search component UI and initial logic

---
+++ src/components/Search.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router';
+
+const Search = () => {
+  const [search, setSearch] = useState('');
+  const navigate = useNavigate();
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(search);
+    navigate(`/search?query=${search}`);
+  };
+
+  return (
+    <form method="get" className="d-flex align-items-center gap-4" onSubmit={handleSearchSubmit}>
+      <label htmlFor="search" className="visually-hidden">
+        Search
+      </label>
+      <input
+        type="text"
+        placeholder="Search And Explore"
+        id="search"
+        className="form-control flex-grow-1"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      <Button variant="warning" type="submit">
+        Search
+      </Button>
+    </form>
+  );
+};
+
+export default Search;
