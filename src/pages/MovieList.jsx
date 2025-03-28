4a3e872d249b0615f502029a4b404a5b2240b59f
belalnagy <belalnajy9@gmail.com>
2025-03-28 13:22:48 +0200

    feat(tvshow): add TV show feature and UI components

--- src/pages/MovieList.jsx
+++ src/pages/MovieList.jsx
@@ -3,28 +3,14 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addToWatchlist, removeFromWatchlist } from '../store/slices/watchlistSlice';
 import { Link } from 'react-router-dom';
 import { FaHeart, FaRegHeart } from 'react-icons/fa';
-
+import WatchListButton from '../components/WatchListButton';
 const MovieList = () => {
-  const dispatch = useDispatch();
-  const watchlist = useSelector((state) => state.watchlist.watchlist); 
   const movies = [
-    { id: 1, title: 'Movie 1' },
-    { id: 2, title: 'Movie 2' },
-    { id: 3, title: 'Movie 3' },
+    { id: 1, title: 'Black Widow ' },
+    { id: 2, title: 'The Good Doctor' },
+    { id: 3, title: 'Dexter' },
+    { id: 4, title: 'The BlackList' },
   ];
-
-
-  const isInWatchlist = (movieId) => {
-    return watchlist.some((item) => item.id === movieId);
-  };
-
-  const handleToggleWatchlist = (movie) => {
-    if (isInWatchlist(movie.id)) {
-      dispatch(removeFromWatchlist(movie.id)); 
-    } else {
-      dispatch(addToWatchlist(movie)); 
-    }
-  };
 
   return (
     <div style={{ padding: '20px' }}>
@@ -42,22 +28,7 @@ const MovieList = () => {
             }}
           >
             <Link style={{ color: 'black', textDecoration: 'none' }}>{movie.title}</Link>
-            <button
-              onClick={() => handleToggleWatchlist(movie)}
-              style={{
-                background: 'none',
-                border: 'none',
-                cursor: 'pointer',
-                padding: '5px',
-              }}
-              aria-label={isInWatchlist(movie.id) ? 'Remove from watchlist' : 'Add to watchlist'}
-            >
-              {isInWatchlist(movie.id) ? (
-                <FaHeart style={{ color: 'red', fontSize: '20px' }} />
-              ) : (
-                <FaRegHeart style={{ color: 'red', fontSize: '20px' }} />
-              )}
-            </button>
+            <WatchListButton movie={movie} />
           </li>
         ))}
       </ul>
