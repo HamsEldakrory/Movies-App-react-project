75ebccba2bea855db04803745cd247b81308fded
belalnagy <belalnajy9@gmail.com>
2025-03-29 13:40:49 +0200

    feat(watchlist): improve empty state UI

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,42 +1,35 @@
 import React from 'react';
 import WatchListCard from '../components/WatchListCard';
-import { useSelector, useDispatch } from 'react-redux';
-import { clearWatchlist } from '../store/slices/watchlistSlice';
-
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../styles/custom.css'
 const WatchList = () => {
-  const dispatch = useDispatch();
   const watchlist = useSelector((state) => state.watchlist.watchlist);
 
   return (
-    <>
-      <div style={{ padding: '20px' }}>
-        <h1>My Watchlist</h1>
-        {watchlist.length === 0 ? (
-          <p>Your watchlist is empty.</p>
-        ) : (
-          <>
-            <button
-              onClick={() => dispatch(clearWatchlist())}
-              style={{
-                marginBottom: '20px',
-                padding: '10px',
-                backgroundColor: '#ff4d4d',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer',
-              }}
-            >
-              Clear Watchlist
-            </button>
-            <div className="d-flex flex-wrap">
-              {watchlist.map((item) => (
-                <WatchListCard key={item.id} movie={item} />
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    </>
+    <div className="watchlist-container" style={{ padding: '40px', textAlign: 'center' }}>
+      <h1 style={{ textAlign: 'left' }}>Watch list</h1>
+      {watchlist.length === 0 ? (
+        <div className="empty-watchlist">
+          <img
+            src="../../public/empty.png"
+            alt="Empty Watchlist"
+            p
+            style={{ width: '400px', marginBottom: '20px' }}
+          />
+          <p style={{ fontSize: '20px' }}>No Movies in watch list</p>
+          <Link to="/" className="back-home-btn btn mt-3"> 
+            Back to home
+          </Link>
+        </div>
+      ) : (
+        <div className="d-flex flex-wrap">
+          {watchlist.map((item) => (
+            <WatchListCard key={item.id} movie={item} />
+          ))}
+        </div>
+      )}
+    </div>
   );
 };
 
