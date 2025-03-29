d2e555fad355869e33c40c44f10cea788be90988
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-29 22:57:35 +0200

    refactor: navbar styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Navbar, Nav, Container } from 'react-bootstrap';
-import { Search, Bookmark } from 'lucide-react';
+import { Search } from 'lucide-react';
+import { FaRegHeart } from 'react-icons/fa';
 import { Link, NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { selectWatchlistCount } from '../store/slices/watchlistSlice';
@@ -21,27 +22,29 @@ const IMDbNavbar = () => {
 
         <Navbar.Collapse id="cinema-navbar-nav">
           <Nav className="me-auto nav-links">
-            <Nav.Link as={NavLink} to="/" className="nav-item">
-              Movies
-            </Nav.Link>
-            <Nav.Link as={NavLink} to="/tv-shows" className="nav-item">
-              TV Shows
-            </Nav.Link>
+            <div className="d-flex gap-3 align-items-center justify-content-center">
+              <Nav.Link as={NavLink} to="/" className="nav-item">
+                Movies
+              </Nav.Link>
+              <Nav.Link as={NavLink} to="/tv-shows" className="nav-item">
+                TV Shows
+              </Nav.Link>
+            </div>
           </Nav>
 
-          <Nav className="ms-auto nav-actions d-flex align-items-center">
-            <Nav.Link as={Link} to="/search" className="nav-icon position-relative">
-              <Search size={30} />
-            </Nav.Link>
-
-            <Nav.Link as={Link} to="/watch-list" className="nav-icon position-relative">
-              <Bookmark size={30} />
-              {watchlistCount > 0 && <span className="badge">{watchlistCount}</span>}
-            </Nav.Link>
-
-            <Nav.Link as={NavLink} className="nav-icon language-drop">
-              <LanguageDropdown />
-            </Nav.Link>
+          <Nav className="ms-auto nav-actions">
+            <div className="d-flex gap-3 align-items-center justify-content-center">
+              <Nav.Link as={Link} to="/search" className="nav-icon position-relative">
+                <Search size={30} />
+              </Nav.Link>
+              <Nav.Link as={Link} to="/watch-list" className="nav-icon position-relative">
+                <FaRegHeart size={30} />
+                {watchlistCount > 0 && <span className="badge">{watchlistCount}</span>}
+              </Nav.Link>
+              <Nav.Link as={NavLink} className="nav-icon language-drop">
+                <LanguageDropdown />
+              </Nav.Link>
+            </div>
           </Nav>
         </Navbar.Collapse>
       </Container>
