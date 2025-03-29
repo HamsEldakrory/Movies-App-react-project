5313b431a4d48a725f882e972b4d3070eecfb052
Ahmed Lebda <59537597+AhmedLebda@users.noreply.github.com>
2025-03-29 21:14:55 +0200

    refactor: watch list page

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,40 +1,31 @@
 import React from 'react';
 import WatchListCard from '../components/WatchListCard';
-import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearWatchlist } from '../store/slices/watchlistSlice';
+import { Button, Container } from 'react-bootstrap';
 const WatchList = () => {
   const watchlist = useSelector((state) => state.watchlist.watchlist);
+  const dispatch = useDispatch();
+
+  if (!watchlist.length) {
+    return (
+      <Container>
+        <h1>My Watchlist</h1>
+        <p>Your watchlist is empty.</p>
+      </Container>
+    );
+  }
+
+  const watchListCards = watchlist.map((item) => <WatchListCard key={item.id} showItem={item} />);
 
   return (
-    <>
-      <div style={{ padding: '20px' }}>
-        <h1>My Watchlist</h1>
-        {watchlist.length === 0 ? (
-          <p>Your watchlist is empty.</p>
-        ) : (
-          <>
-            <button
-              onClick={() => dispatch(clearWatchlist())}
-              style={{
-                marginBottom: '20px',
-                padding: '10px',
-                backgroundColor: '#ff4d4d',
-                color: 'white',
-                border: 'none',
-                cursor: 'pointer',
-              }}
-            >
-              Clear Watchlist
-            </button>
-            <div className="d-flex flex-wrap">
-              {watchlist.map((item) => (
-                <WatchListCard key={item.id} showItem={item} />
-              ))}
-            </div>
-          </>
-        )}
-      </div>
-    </>
+    <Container>
+      <h1>My Watchlist</h1>
+      <Button onClick={() => dispatch(clearWatchlist())} className="custom-button">
+        Clear Watchlist
+      </Button>
+      <div className="d-flex flex-wrap">{watchListCards}</div>
+    </Container>
   );
 };
 
